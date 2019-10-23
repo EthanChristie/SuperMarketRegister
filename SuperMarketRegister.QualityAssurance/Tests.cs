1865f7be4c370c8552bc0679cc67b74ff7ad3751
Ethan Christie <ethan.j.christie@outlook.com>
2019-10-24 09:02:12 +1000

    Add groceries test

--- SuperMarketRegister.QualityAssurance/Tests.cs
+++ SuperMarketRegister.QualityAssurance/Tests.cs
@@ -19,6 +19,32 @@ Tax (10%) = $0.25
 Total = $2.75";
             Assert.AreEqual(expected, receipt.ToString());
         }
+
+        [Test]
+        public void TestGroceries()
+        {
+            var receipt = new Receipt();
+            receipt.AddItem(1, "Cucumber", 1);
+            receipt.AddItem(2, "2L Cola", 0.89m);
+            receipt.AddItem(5, "Coconut", 1.50m);
+            receipt.AddItem(1, "Punnet cherry tomatoes", 3);
+            receipt.AddItem(5, "Banana", 0.8m);
+            receipt.AddItem(2, "Chicken breast", 1.77m);
+            receipt.AddItem(5, "Tin baked beans", 0.79m);
+
+            var expected =
+@"1 Cucumber @ $1.00 = $1.00
+2 2L Cola @ $0.89 = $1.78
+5 Coconut @ $1.50 = $7.50
+1 Punnet cherry tomatoes @ $3.00 = $3.00
+5 Banana @ $0.80 = $4.00
+2 Chicken breast @ $1.77 = $3.54
+5 Tin baked beans @ $0.79 = $3.95
+SubTotal = $24.77
+Tax (10%) = $2.48
+Total = $27.25";
+            Assert.AreEqual(expected, receipt.ToString());
+        }
     }
 
     
